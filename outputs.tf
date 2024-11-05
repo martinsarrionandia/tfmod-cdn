477f3569ac306da3cd12f779fcadb2a43672af4a
Martin Sarrionandia <martin@sarrionandia.co.uk>
2024-11-05 23:22:08 +0000

    Add output bucket region

--- outputs.tf
+++ outputs.tf
@@ -1,7 +1,11 @@
-output "secret_arn" {
+output "secret-arn" {
   value = resource.aws_secretsmanager_secret_version.this-current.arn
 }
 
-output "cdn_bucket_name" {
+output "cdn-bucket-name" {
   value = local.fqdn
+}
+
+output "cdn-bucket-region" {
+  value = resource.aws_s3_bucket.this.region
 }
